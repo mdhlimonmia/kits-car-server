baa6bf145678e7fb8a2535ff18a170aa98959b49
Mdhlimon <mdhlimonmia@gmail.com>
2023-05-21 15:58:25 +0600

    get data by email setup

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const corsConfig = {
-  origin: "",
+  origin: "*",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  optionSuccessStatus: 200,
+  // methods: ['GET', 'POST', 'PUT', 'DELETE']
 };
 app.use(cors(corsConfig));
-app.options("", cors(corsConfig));
+app.options("*", cors(corsConfig));
+// app.use(cors())
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pk85uor.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // client.connect();
     // Send a ping to confirm a successful connection
     const carCollection = client.db("kitsCar").collection("cars");
 
@@ -36,6 +38,33 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/category", async (req, res) => {
+      // const decoded = req.decoded;
+      // console.log('came back after verify', decoded)
+
+      // if(decoded.email !== req.query.email){
+      //     return res.status(403).send({error: 1, message: 'forbidden access'})
+      // }
+      console.log(req.query.category);
+
+      let query = {};
+      if (req.query?.category) {
+        query = { sub_category: req.query.category };
+      }
+      const result = await carCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get('/mycars', async (req, res) => {
+      console.log(req.query.seller_email);
+      let query = {};
+      if (req.query?.seller_email) {
+          query = { seller_email: req.query.seller_email }
+      }
+      const result = await carCollection.find(query).toArray();
+      res.send(result);
+  })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
